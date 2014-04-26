45009c5156dd5766f8c6fc417515a4ae3b842c98
Elton Chung <elbo1216@gmail.com>
2014-04-26 17:16:45 -0400

    Problem 5 Solved

--- scala/Problem5.scala
+++ scala/Problem5.scala
@@ -4,3 +4,29 @@
  **/
 
 // vim: set ts=4 sw=4 et:
+def getFactors(num: Long): List[Long] = {
+  num match {
+    case 1 => List(num)
+    case n:Long => {
+      var div:Long = 2
+      while (num % div > 0)
+        div = div + 1
+      List(div) ++ getFactors(num/div)
+    }
+  }
+}
+
+def LCM(nums: List[Long]): Long = {
+  nums.map(getFactors(_)).foldLeft(Map():Map[Long, List[Long]]) { (countMap, factors) => 
+    val mapFactors = factors.groupBy( x => x )
+    countMap.foldLeft(Map():Map[Long, List[Long]]) { (m, item) =>
+      val value = mapFactors.get(item._1) match {
+        case Some(factorList) if (factorList.size > item._2.size) => factorList
+        case _ => item._2
+      }
+      m ++ Map(item._1 -> value)
+    } ++ mapFactors.filterKeys(key => !countMap.keys.toList.contains(key))
+  }.values.flatten.product
+}
+
+println(LCM((1.toLong to 20.toLong).toList))
