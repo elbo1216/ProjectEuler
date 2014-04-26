7e66c241b6c41519ea2de1365b91047fbe777754
Elton Chung <elbo1216@gmail.com>
2014-04-26 18:20:14 -0400

    Problem 7 solved

--- scala/Problem7.scala
+++ scala/Problem7.scala
@@ -3,4 +3,23 @@
   *  What is the 10001st prime number?
 **/
 
-// vim: set ts=4 sw=4 et:
+def getNthPrime(nth: Int): Long = {
+  (1 to nth-1).foldLeft(2.toLong)( (nthPrime, iter) => getNextPrime(nthPrime) )
+}
+
+def getNextPrime(p: Long): Long = {
+  isPrime(p+1) match {
+    case false =>  getNextPrime(p+1)
+    case true => p+1
+  }
+}
+
+def isPrime(num: Long): Boolean = {
+  num match {
+    case 2 => true
+    case 3 => true
+    case _ => (2.toLong to scala.math.sqrt(num).toLong).toList.filter(i => num % i == 0).size == 0
+  }
+}
+
+println(getNthPrime(1001))
